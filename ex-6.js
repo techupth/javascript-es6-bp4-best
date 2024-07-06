ff0a44fe4227213383856ed567044b73be269e0d
Thammalak <thammalak.ti@gmail.com>
2024-07-06 23:28:41 +0700

    summit

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,9 @@
 let height = undefined;
 let result;
 
+function checknull(cnull){
+    return cnull ?? "Height is not defined";
+} 
+
+result = checknull(height)
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
